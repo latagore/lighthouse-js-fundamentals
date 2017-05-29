03e35ddd5759e874b75666ab2643fb5d40382e03
Steven Xu <s@stevenxu.xyz>
2017-05-29 18:37:29 +0000

    refactor loopyLighthouse to be cleaner

--- loopyLighthouse2.js
+++ loopyLighthouse2.js
@@ -1,13 +1,13 @@
 function loopyLighthouse(range, multiples, words) {
   for (var i = range[0]; i <= range[1]; i++) {
-    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
-      console.log(words[0] + words[1]);
-    } else if (i % multiples[0] === 0) {
-      console.log(words[0]);
-    } else if (i % multiples[1] === 0) {
-      console.log(words[1]);
-    } else {
-      console.log(i);
+    var result = "";
+    if (i % multiples[0] === 0) {
+      result += words[0];
     }
+    if (i % multiples[1] === 0) {
+      result += words[1];
+    }
+    
+    console.log(result || i);
   }
 }
